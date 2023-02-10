25b77187e0e3233d56b420b2ee85dc6f5daae4c5
Kasiet <bloodyangel10k@gmail.com>
2023-02-10 16:26:42 +0600

    HW7

--- Afisha/settings.py
+++ Afisha/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import rest_framework.authentication
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@%2k+k(+)v_4+2(&n!uqr359+)ip10pbmaykl_s4-v1+u*09#u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'Afisha.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
--- Afisha/urls.py
+++ Afisha/urls.py
@@ -20,6 +20,6 @@ from users import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("movie_app.urls")),
+    path('api/v1/', include('movie_app.urls')),
     path('api/v1/', include('users.urls')),
 ]
